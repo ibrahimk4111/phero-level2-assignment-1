f96aec223a0be499b784b918ad3daa32bee4b718
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-05-10 14:28:36 +0600

    class object/js file fixs

--- dist/Problem-8.carObject.js
+++ dist/Problem-8.carObject.js
@@ -1,12 +1,15 @@
 "use strict";
 {
-  class Car {
-    constructor(public brand: string, public model: string, public year : number ){}
-    carDetails(){
-        console.log (`Your car model is: ${this.year} ${this.brand} ${this.model}`)
+    class Car {
+        constructor(brand, model, year) {
+            this.brand = brand;
+            this.model = model;
+            this.year = year;
+        }
+        carDetails() {
+            console.log(`Your car model is: ${this.year} ${this.brand} ${this.model}`);
+        }
     }
-}
-
-const toyota = new Car ("Toyota", "Corolla", 2020)
-toyota.carDetails()
+    const toyota = new Car("Toyota", "Corolla", 2020);
+    toyota.carDetails();
 }
